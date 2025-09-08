e3bc388bfcb8bd0b08c10d83ed707b197ff932ae
berale <b.y.berale@gmail.com>
2025-09-08 10:36:58 +0300

    easy changes: changing the name of one of data_retrival component

--- app/services/data_retrieval/data_retrieval_service.py
+++ app/services/data_retrieval/data_retrieval_service.py
@@ -1,19 +1,16 @@
-from importlib.metadata import metadata
 import os
-#
-# from app.models import Producer
+from app.models import Producer
 from app.services.data_retrieval.conigurations import JSON_SCHEMA, KAFKA_TOPIC, DIRECTORY_PATH
-from app.services.data_retrieval.data_retrieval_manager import DataRetrievalManager
+from app.services.data_retrieval.data_retrieval_helper import DataRetrievalHelper
 
 
 
 class DataRetrievalService:
     def __init__(self):
         self._directory_path = DIRECTORY_PATH
-        # self._file_path_template = FILE_PATH_TEMPLATE #The files are numbered - I made a template for them, when running on the loop the template will be adjusted to a specific file
         self._kafka_topic = KAFKA_TOPIC
-        # self._producer = Producer()
-        self._manager = DataRetrievalManager
+        self._producer = Producer()
+        self._manager = DataRetrievalHelper
 
 
     def send_metadata(self):
@@ -21,7 +18,7 @@ class DataRetrievalService:
         for entry in os.listdir(self._directory_path):
             specific_meta_data = self._manager.get_specific_metadata(os.path.join(self._directory_path, entry))
             print(specific_meta_data)
-            # self._producer.publish_massage(self._kafka_topic, specific_meta_data)
+            self._producer.publish_massage(self._kafka_topic, specific_meta_data)
 
 
 
